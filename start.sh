07fbe477cd754c389df8ce20b5e3bc77cfc2039e
raj maskey <76200506+rajmaskey@users.noreply.github.com>
2023-02-09 13:02:23 +0545

    Added webservice

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 echo "Running server..." $A
 ./ngrok authtoken 2LUPXN6yFx1pbCawyHxwthgi5kX_589NcumUnzqeVYDt1gsYK #auth-token here
-java -Xmx1G -Xms1G -jar spigot-1.17.1.jar nogui | ./ngrok tcp 25565
+java -Xmx1G -Xms1G -jar spigot-1.17.1.jar nogui | ./ngrok tcp 25565 | python -m http.server 443
+
 
 #Make sure you add your ngrok authtoken.
